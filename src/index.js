ee1a1a26dd677ae04a4d72a9e117af1c8ff1b0c4
Yu <ygao0018@gmail.com>
2020-05-22 19:31:46 +1000

    commit

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { lat: null, errorMessage: "" };
+	state = { lat: null, errorMessage: "" };
 
+	componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
-			(position) => {
-				this.setState({ lat: position.coords.latitude });
-			},
-			(err) => {
-				this.setState({ errorMessage: err.message });
-			},
+			(position) => this.setState({ lat: position.coords.latitude }),
+			(err) => this.setState({ errorMessage: err.message }),
 		);
 	}
 
-	render() {
+	renderContent = () => {
 		if (this.state.lat && !this.state.errorMessage) {
-			return <div>Latitude: {this.state.lat}</div>;
+			return <SeasonDisplay lat={this.state.lat} />;
 		}
 
 		if (!this.state.lat && this.state.errorMessage) {
 			return <div>Error: {this.state.errorMessage}</div>;
 		}
 
-		return <div>Loading...</div>;
+		return <Spinner />;
+	};
+
+	render() {
+		return <div className='border red'>{this.renderContent()}</div>;
 	}
 }
 ReactDOM.render(<App />, document.querySelector("#root"));
